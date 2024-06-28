02a2baba6317414a3690d95bc956c4daf38306e0
vikash chand <vikash.chand2020@vitstudent.ac.in>
2024-06-28 09:03:26 +0530

    added dashboard ui changes s

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,7 +1,7 @@
-// const baseUrl = 'https://police-portal-back.vercel.app';
-// export default baseUrl;
-
-const baseUrl = 'http://localhost:5000';
+const baseUrl = 'https://police-portal-back.vercel.app';
 export default baseUrl;
+
+// const baseUrl = 'http://localhost:5000';
+// export default baseUrl;
 
 // const k = "hi";
